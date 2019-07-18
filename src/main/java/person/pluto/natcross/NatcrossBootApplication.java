f6fbdaa3bc0c01342deaecc979aae85c5aa71799
王敏 <wanmgin1994@qq.com>
2019-07-18 09:39:45 +0800

    first

---
+++ src/main/java/person/pluto/natcross/NatcrossBootApplication.java
@@ -0,0 +1,13 @@
+package person.pluto.natcross;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NatcrossBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NatcrossBootApplication.class, args);
+	}
+
+}
