4c58e84b3158ba95b267e14244ad7979f4f35821
Pluto <wangmin07@hotmail.com>
2019-07-19 16:59:43 +0800

    修复NatcrossSpringConstants不能正常注入的问题，缓存4096快了好多呀！！！！！！

--- src/main/java/person/pluto/natcross/bean/NatcrossSpringConstants.java
+++ src/main/java/person/pluto/natcross/bean/NatcrossSpringConstants.java
@@ -19,22 +19,22 @@ import org.springframework.stereotype.Component;
 @ConfigurationProperties(prefix = "natcross.constants")
 public class NatcrossSpringConstants {
 
-    public static void setSTREAM_CACHE_SIZE(Integer sTREAM_CACHE_SIZE) {
+    public void setSTREAM_CACHE_SIZE(Integer sTREAM_CACHE_SIZE) {
         NatcrossConstants.STREAM_CACHE_SIZE = sTREAM_CACHE_SIZE;
         log.trace("STREAM_CACHE_SIZE[{}]", NatcrossConstants.STREAM_CACHE_SIZE);
     }
 
-    public static void setCLIENT_SERVER_PORT(Integer cLIENT_SERVER_PORT) {
+    public void setCLIENT_SERVER_PORT(Integer cLIENT_SERVER_PORT) {
         NatcrossConstants.CLIENT_SERVER_PORT = cLIENT_SERVER_PORT;
         log.trace("CLIENT_SERVER_PORT[{}]", NatcrossConstants.CLIENT_SERVER_PORT);
     }
 
-    public static void setCLIENT_HEART_INTERVAL(Long cLIENT_HEART_INTERVAL) {
+    public void setCLIENT_HEART_INTERVAL(Long cLIENT_HEART_INTERVAL) {
         NatcrossConstants.CLIENT_HEART_INTERVAL = cLIENT_HEART_INTERVAL;
         log.trace("CLIENT_HEART_INTERVAL[{}]", NatcrossConstants.CLIENT_HEART_INTERVAL);
     }
 
-    public static void setTRY_RECLIENT_COUNT(Integer tRY_RECLIENT_COUNT) {
+    public void setTRY_RECLIENT_COUNT(Integer tRY_RECLIENT_COUNT) {
         NatcrossConstants.TRY_RECLIENT_COUNT = tRY_RECLIENT_COUNT;
         log.trace("TRY_RECLIENT_COUNT[{}]", NatcrossConstants.TRY_RECLIENT_COUNT);
     }
