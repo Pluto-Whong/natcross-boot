965a5b4af5eddac21dd94f6cb41123cf4aa06ea2
Pluto <wangmin07@hotmail.com>
2020-01-15 11:36:54 +0800

    打包前 install  natcross2

--- doc/install.sh
+++ doc/install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+rm -rf ./natcross2
+git clone https://github.com/Pluto-Whong/natcross2.git ./natcross2
+mvn clean compile install -Dmaven.test.skip=true -f ./natcross2
+
 rm -rf ./source_code
 
 git clone https://github.com/Pluto-Whong/natcross-boot.git ./source_code
